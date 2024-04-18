1bcbe21b8f8fd3b41f16ca8b2384b03ac98176be
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-04-18 13:01:59 +0000

    Domain Name Generator(basic)

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".net", ".org"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          domainName.push(` ${pronoun[a]} ${adj[b]} ${noun[c]} ${domain[d]}`);
+        }
+      }
+    }
+  }
+
+  document.querySelector("#namegenerator").innerHTML = domainName;
 };
